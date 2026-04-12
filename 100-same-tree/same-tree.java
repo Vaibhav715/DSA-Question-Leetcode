12b3f5f6eda09578ca9073d77a2f66047a4fc6c4
Vaibhav Srivastava <vaibhavsrivastava7715@gmail.com>
2026-04-12 18:39:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (76.46%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,21 +15,34 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        List<Integer> tree1 = new ArrayList<>();
-        List<Integer> tree2 = new ArrayList<>();
-         tree1 = preorder(p, tree1);
-         tree2 = preorder(q, tree2);
-        return tree1.equals(tree2);
+    //     List<Integer> tree1 = new ArrayList<>();
+    //     List<Integer> tree2 = new ArrayList<>();
+    //      tree1 = preorder(p, tree1);
+    //      tree2 = preorder(q, tree2);
+    //     return tree1.equals(tree2);
 
-    }
-    List<Integer> preorder(TreeNode root, List<Integer> tree){
-        if(root == null){
-        tree.add(null);
-        return tree;
-        }
-        tree.add(root.val);
-        preorder(root.left, tree);
-        preorder(root.right, tree);
-        return tree;
-    }
+    // }
+    // List<Integer> preorder(TreeNode root, List<Integer> tree){
+    //     if(root == null){
+    //     tree.add(null);
+    //     return tree;
+    //     }
+    //     tree.add(root.val);
+    //     preorder(root.left, tree);
+    //     preorder(root.right, tree);
+    //     return tree;
+    // }
+
+
+    if(p == null && q == null)
+    return true;
+
+    if(p == null || q == null)
+    return false;
+
+    if(p.val != q.val)
+    return false;
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
 }
