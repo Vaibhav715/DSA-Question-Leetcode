12f17166309a70ba983e74d5cd5996b979ff74f5
Vaibhav Srivastava <vaibhavsrivastava7715@gmail.com>
2026-08-17 10:33:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (21.56%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,20 +10,54 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-      ListNode DummyNode = new ListNode(0);  
-      DummyNode.next = head;
-      ListNode Slow = DummyNode;
-      ListNode Fast = DummyNode;
+    //   ListNode DummyNode = new ListNode(0);  
+    //   DummyNode.next = head;
+    //   ListNode Slow = DummyNode;
+    //   ListNode Fast = DummyNode;
 
-      for(int i = 0 ; i<=n ; i++){
-        Fast = Fast.next;
-      }
+    //   for(int i = 0 ; i<=n ; i++){
+    //     Fast = Fast.next;
+    //   }
 
-      while(Fast != null){
-        Fast = Fast.next;
-        Slow = Slow.next;
-      }
-      Slow.next = Slow.next.next;
-      return DummyNode.next;
+    //   while(Fast != null){
+    //     Fast = Fast.next;
+    //     Slow = Slow.next;
+    //   }
+    //   Slow.next = Slow.next.next;
+    //   return DummyNode.next;
+
+
+
+
+        if(head == null) {//If there is no linked list then there is nothing to remove
+            return null;
+        }
+	//two pointers to traverse over the linked list with a gap of n
+        ListNode prev = head;
+        ListNode curr = head;
+	//creating the gap of n between prev and the curr pointer
+        for(int i = 0; i < n; i++) {
+            curr = curr.next;
+        }
+	//if curr is null, it means the question has asked us to remove the first node
+	//get the address of the second node and return the address
+        if(curr == null) {
+            ListNode nh = head.next;
+            head.next = null;
+            head = nh;
+            return head;
+        }
+	//keep traversing the curr pointer till the end until the prev pointer reach the node address just behind the one to remove
+        while(curr.next != null) {
+            prev = prev.next;
+            curr = curr.next;
+        }
+ 
+	//remove the node
+        ListNode n1 = prev.next;
+        ListNode n2 = n1.next;
+        n1.next = null;
+        prev.next = n2;
+        return head;
     }
 }
