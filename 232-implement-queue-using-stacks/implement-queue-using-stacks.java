3da0f6bb5995b92f1aafdc56ab08b5c67a32a981
Vaibhav Srivastava <vaibhavsrivastava7715@gmail.com>
2026-09-01 15:43:09 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (15.23%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,43 +1,94 @@
+// class MyQueue {
+//     Stack<Integer> s1;
+//     Stack<Integer> s2;
+//     public MyQueue() {
+//         s1 = new Stack<>();
+//         s2 = new Stack<>();
+//     }
+    
+//     public void push(int x) {
+
+//         while(!s1.isEmpty()){
+//            int val =  s1.pop();
+//             s2.push(val);
+//         }
+//         s1.push(x);
+//          while(!s2.isEmpty()){
+//            int val =  s2.pop();
+//             s1.push(val);
+//         }
+//     }
+    
+//     public int pop() {
+//         if(s1.isEmpty()){
+//             return -1;
+//         }
+//         int value = s1.pop();
+//         return value;
+//     }
+    
+//     public int peek() {
+//          if(s1.isEmpty()){
+//             return -1;
+//         }
+//         return s1.peek();
+//     }
+    
+//     public boolean empty() {
+//         return (s1.isEmpty() && s2.isEmpty());
+//     }
+// }
+
+
+import java.util.Stack;
+
 class MyQueue {
-    Stack<Integer> s1;
-    Stack<Integer> s2;
+    private Stack<Integer> s1; // Input stack for push operations
+    private Stack<Integer> s2; // Output stack for pop/peek operations
+
     public MyQueue() {
         s1 = new Stack<>();
         s2 = new Stack<>();
     }
     
+    // O(1) Time complexity
     public void push(int x) {
-
-        while(!s1.isEmpty()){
-           int val =  s1.pop();
-            s2.push(val);
-        }
         s1.push(x);
-         while(!s2.isEmpty()){
-           int val =  s2.pop();
-            s1.push(val);
-        }
     }
     
+    // Amortized O(1) Time complexity
     public int pop() {
-        if(s1.isEmpty()){
+        if (empty()) {
             return -1;
         }
-        int value = s1.pop();
-        return value;
+        shiftStacks();
+        return s2.pop();
     }
     
+    // Amortized O(1) Time complexity
     public int peek() {
-         if(s1.isEmpty()){
+        if (empty()) {
             return -1;
         }
-        return s1.peek();
+        shiftStacks();
+        return s2.peek();
     }
     
+    // O(1) Time complexity
     public boolean empty() {
-        return (s1.isEmpty() && s2.isEmpty());
+        return s1.isEmpty() && s2.isEmpty();
+    }
+    
+    // Helper to transfer elements only when s2 is empty
+    private void shiftStacks() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
     }
 }
+
 
 /**
  * Your MyQueue object will be instantiated and called as such:
