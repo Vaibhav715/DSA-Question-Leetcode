0f117f0753144cd0bd261afa17fbe6aeb70d310a
Vaibhav Srivastava <vaibhavsrivastava7715@gmail.com>
2026-09-21 21:00:21 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (67.14%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -3,7 +3,7 @@ class Solution {
         int value = 0;
         for(int i = 0; i<s.length(); i++){
             char ch = s.charAt(i);
-             value = value + (('z' - ch + 1)*(i+1)) ;
+             value = value + (( 26 - (ch - 'a'))*(i+1));
         }
         return value;
     }
